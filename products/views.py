316cc39674641447883f43b9c35e359e2ee4c599
jeum <eumjungmin1@gmail.com>
2022-07-21 19:51:32 +0900

    Add: ProductDetailView

--- products/views.py
+++ products/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http  import JsonResponse
+from django.views import View
+
+from .models import Product
+
+class ProductDetailView(View):
+    def get(self, request, product_id):
+        product = Product.objects.get(id=product_id)
+
+        image_list = []
+        images = product.productimage_set.all()
+        for image in images:
+            image_list.append({'img_url' : image.url})
+
+        option_list = []
+        options = product.options.all()
+        for option in options:
+            option_list.append(option.name)
+        
+
+        result = {
+            'id'      : product.id,
+            'name'    : product.name,
+            'price'   : product.price,
+            'is_green': product.is_green,
+            'is_best' : product.is_best,
+            'img_urls': image_list,
+            'options' : option_list
+        }
+
+        return JsonResponse({'result': result}, status=200)
