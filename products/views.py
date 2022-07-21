77d7221c2f7520190c97d778acfd8fe966955bd1
jeum <eumjungmin1@gmail.com>
2022-07-21 20:49:06 +0900

    Add: ProductDetailView

--- products/views.py
+++ products/views.py
@@ -7,27 +7,21 @@ from .models import Product
 
 class ProductDetailView(View):
     def get(self, request, product_id):
-        product = Product.objects.get(id=product_id)
+        try:
+            product = Product.objects.get(id=product_id)
 
-        image_list = []
-        images = product.productimage_set.all()
-        for image in images:
-            image_list.append({'img_url' : image.url})
+            images  = product.productimage_set.all()
+            options = product.options.all()
 
-        option_list = []
-        options = product.options.all()
-        for option in options:
-            option_list.append(option.name)
-        
-
-        result = {
-            'id'      : product.id,
-            'name'    : product.name,
-            'price'   : product.price,
-            'is_green': product.is_green,
-            'is_best' : product.is_best,
-            'img_urls': image_list,
-            'options' : option_list
-        }
-
-        return JsonResponse({'result': result}, status=200)
+            result = {
+                'id'      : product.id,
+                'name'    : product.name,
+                'price'   : product.price,
+                'is_green': product.is_green,
+                'is_best' : product.is_best,
+                'img_urls': [image.url for image in images],
+                'options' : [option.name for option in options]
+            }
+            return JsonResponse({'result': result}, status=200)
+        except Product.DoesNotExist:
+            return JsonResponse({'message': 'PRODUCT_DOES_NOT_EXIST'}, status=400)
