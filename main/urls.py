6aed40bb65bdb353620c003996856715b08c1046
jeum <eumjungmin1@gmail.com>
2022-07-22 11:31:34 +0900

    Add: main app

---
+++ main/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+# from main.views import ProductListView
+
+# urlpatterns = {
+#     path('', ProductListView.as_view()),
+# }
---
+++ main/views.py
@@ -0,0 +1,35 @@
+from django.http  import JsonResponse
+from django.views import View
+
+from .models import Product, Category
+
+class MainView(View):
+    def get(self, request, cat_id=None):
+        result = []
+
+        # 전체리스트
+        if cat_id == None:
+            products = Product.objects.all()
+
+        #카테고리별
+        else:   
+            products = Product.objects.filter(category_id=cat_id)
+
+            if not Category.objects.filter(id=cat_id).exists():
+                return JsonResponse({'message':'CATEGORY_DOES_NOT_EXIST'}, status=400)
+
+        result.append({'total': products.count()})
+
+        for product in products:
+            images = product.productimage_set.all()
+            product_information = {
+                'id'      : product.id,
+                'name'    : product.name,
+                'price'   : product.price,
+                'is_green': product.is_green,
+                'is_best' : product.is_best,
+                'img_urls': [image.url for image in images]
+            }
+            result.append(product_information)
+
+        return JsonResponse({'result': result}, status=200)
