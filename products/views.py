f8bd9b1f1066131e3da9cf1252b9dc8268e7f9fa
jeum <eumjungmin1@gmail.com>
2022-07-22 12:00:02 +0900

    Add: id for options and images

--- products/views.py
+++ products/views.py
@@ -17,8 +17,14 @@ class ProductDetailView(View):
                 'price'   : product.price,
                 'is_green': product.is_green,
                 'is_best' : product.is_best,
-                'img_urls': [image.url for image in images],
-                'options' : [option.name for option in options]
+                'images'  : [{
+                    'id' : image.id,
+                    'url': image.url
+                    } for image in images],
+                'options' : [{
+                    'id'  : option.id,
+                    'name': option.name
+                    } for option in options]
             }
             return JsonResponse({'result': result}, status=200)
         except Product.DoesNotExist:
