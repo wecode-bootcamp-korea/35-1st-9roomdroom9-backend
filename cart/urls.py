035300abad5d65bc0e3e6d649cb85e0e3b6b5e75
fkelfk <fkelfk09@gmail.com>
2022-07-23 16:38:22 +0900

    wip

--- cart/urls.py
+++ cart/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from .views import CartCreateView
+from .views import CartView
 
 urlpatterns = [
-    path('/cart', CartCreateView.as_view()),
+    path('', CartView.as_view()),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,22 +1,54 @@
-from itertools import product
 import json
-from statistics import quantiles
 
 from django.views import View
 from products.models import *
 from orders.models import Cart
-
+from products.models import ProductOption
 from django.http  import JsonResponse
 
-class CartCreateView(View):
+class CartView(View):
     def post(self, request):
         try:
-
+            user_id = request.user.id
+            product_option_id = request.product_option_id
             data = json.loads(request.body)
-            product = [product for product in ProductOption.objects.all()]
-            
-            print(ProductOption)
+            quantity = data['quantity']
 
-           
-        except KeyError:
-            return JsonResponse({'message': 'KEY_ERROR'}, status=400)
+            if Cart.objects.filter(pk = product_option_id).exists():
+                cart = Cart.objects.get(pk=product_option_id)
+                cart.quantity += quantity
+                cart.save()
+                
+                return JsonResponse({'message' : 'CREATE_CART'}, status=201)
+
+            Cart.objects.create(
+                quantity = quantity,
+                user_id = user_id,
+                product_option_id = product_option_id,
+            )
+
+            return JsonResponse({'message': 'sucess'}, status=200)
+
+        except KeyError as error:
+            return JsonResponse({'message': f'{error}'}, status=400)
+
+    # def get(self, request):
+    #     try:
+    #         user = request.user
+    #         print(cart)
+    #         result = []
+    #         for cart in carts:
+    #             result.append({
+    #                 "cart_id" : cart.id,
+    #                 "product"  : cart.product.name,
+    #                 "product_image_1" : cart.product.name,
+    #                 "price" : cart.product.price,
+    #                 "quantity" : cart.quantity,
+    #                 "product_id" : cart.product.id
+    #                 })
+
+    #         return JsonResponse({'result': result }, status=200)
+
+    #     except KeyError as error:
+    #         return JsonResponse({'message': f'{error}'}, status=400)
+
