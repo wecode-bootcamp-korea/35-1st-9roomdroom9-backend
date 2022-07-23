4b7e7dede3d9cd19a284fe91ed7829968d4e66c0
jeum <eumjungmin1@gmail.com>
2022-07-23 18:05:33 +0900

    Add: sort product list

--- products/views.py
+++ products/views.py
@@ -5,36 +5,47 @@ from .models import Product, Category
 
 class ProductListView(View):
     def get(self, request, category_id=1000):
-        if not Category.objects.filter(id=category_id).exists(): 
-            return JsonResponse({'message':'CATEGORY_DOES_NOT_EXIST'}, status=400)
+        try:
+            if not Category.objects.filter(id=category_id).exists(): 
+                return JsonResponse({'message':'CATEGORY_DOES_NOT_EXIST'}, status=400)
 
-        category = Category.objects.get(id=category_id)
-        products = Product.objects.all()
+            category = Category.objects.get(id=category_id)
+            products = Product.objects.all()
 
-        if category_id != 1000:
-            products = Product.objects.filter(category_id=category_id)
-
-        category_data = {
-            'id'            : category.id,
-            'description'   : category.description,
-            'total_products': products.count()
+            if category_id != 1000:
+                products = Product.objects.filter(category_id=category_id)
+            
+            sort_by = {
+                None        : 'id',
+                'NEW'       : '-created_at',
+                'HIGH_PRICE': 'price',
+                'LOW_PRICE' : '-price'
             }
+            products = products.order_by(sort_by[request.GET.get('sorting', None)])
 
-        products_data = [{
-                'id'      : product.id,
-                'name'    : product.name,
-                'price'   : product.price,
-                'is_green': product.is_green,
-                'is_best' : product.is_best,
-                'images'  : [{
-                    'id' : image.id,
-                    'url': image.url
-                    } for image in product.productimage_set.all()]
-            } for product in products]
+            category_data = {
+                'id'            : category.id,
+                'description'   : category.description,
+                'total_products': products.count()
+                }
 
-        return JsonResponse({
-            'products_data' : products_data,
-            'category_data' : category_data}, status=200)
+            products_data = [{
+                    'id'      : product.id,
+                    'name'    : product.name,
+                    'price'   : product.price,
+                    'is_green': product.is_green,
+                    'is_best' : product.is_best,
+                    'images'  : [{
+                        'id' : image.id,
+                        'url': image.url
+                        } for image in product.productimage_set.all()]
+                } for product in products]
+
+            return JsonResponse({
+                'products_data' : products_data,
+                'category_data' : category_data}, status=200)
+        except KeyError as e:
+            return JsonResponse({'message':f'KEY_ERROR {e}'}, status=400)
 
 class ProductDetailView(View):
     def get(self, request, product_id):
