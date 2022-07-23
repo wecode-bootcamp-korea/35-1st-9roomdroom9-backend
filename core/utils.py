bdac3baebbed8dcc73a6386b51b68a35a45f062c
fkelfk <fkelfk09@gmail.com>
2022-07-24 02:11:24 +0900

    wip

---
+++ core/utils.py
@@ -0,0 +1,67 @@
+import re , jwt , bcrypt
+
+from functools   import wraps
+from django.conf import settings
+from django.http import JsonResponse
+from django.core.exceptions import ObjectDoesNotExist
+
+from users.models import User
+
+def vaildNameRegex(value):
+    REGEX_NAME     = '^[가-힣]{2,5}$'
+    if not re.match(REGEX_NAME, value):
+        raise ValueError("INVALID_NAME")
+
+def validPasswordRegex(value):
+    REGEX_PASSWORD = '^(?=.*[A-Za-z])(?=.*\d)(?=.*[?!@#$%*&])[A-Za-z\d?!@#$%*&]{8,}$'
+    if not re.match(REGEX_PASSWORD, value):
+        raise ValueError("INVALID_PASSWORD")
+
+def validEmailRegex(value):
+    REGEX_EMAIL    = '^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+    if not re.match(REGEX_EMAIL, value):
+        raise ValueError("INVALID_EMAIL")
+
+def validBirthdayRegex(value):
+    REGEX_BIRTHDAY = '^(19[0-9][0-9]|20\d{2})-(0[0-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$'
+    if not re.match(REGEX_BIRTHDAY, value):
+        raise ValueError("INVALID_BIRTHDAY")
+
+def validMobileRegex(value):
+    REGEX_MOBILE   = '^\d{3}-\d{3,4}-\d{4}$'
+    if not re.match(REGEX_MOBILE, value):
+        raise ValueError("INVALID_MOBILE_NUMBER")
+
+def checkEmailExist(value):
+    if User.objects.filter(email = value).exists():
+        raise ObjectDoesNotExist("EXIST_EMAIL")
+
+def checkMobileExist(value):
+    if User.objects.filter(mobile_number = value).exists():
+        raise ObjectDoesNotExist("EXIST_MOBILE_NUMBER")
+
+def checkPassword(incomePw, recordedPw):
+    encoded_password = incomePw.encode('utf-8')
+    user_password = recordedPw.encode('utf-8')
+    if not bcrypt.checkpw(encoded_password, user_password):
+        raise ValueError("INVALID_USER")
+    
+def accessCkecked(func):
+    @wraps(func)
+    def wrapper(self, request, *args, **kwargs):
+        try:
+            access_token = request.headers.get('Authorization', None)
+            payload      = jwt.decode(access_token, settings.SECRET_KEY, settings.ALGORITHM)
+            user         = User.objects.get(id = payload['id'])
+            request.user = user
+        
+        except jwt.exceptions.DecodeError:
+            return JsonResponse({'message' : 'INVALID TOKEN'}, status = 400)
+        
+        except User.DoesNotExist:
+            return JsonResponse({'message' : 'INVALID USER'}, status = 400)
+
+        return func(self, request, *args, **kwargs)
+
+    return wrapper
+
--- users/views.py
+++ users/views.py
@@ -1,3 +1,74 @@
-from django.shortcuts import render
+import json, jwt
 
-# Create your views here.
+from django.http  import JsonResponse
+from django.views import View
+from django.conf  import settings
+from django.core.exceptions import ObjectDoesNotExist
+
+from .models import User
+from core.utils import *
+
+
+class SignUpView(View):
+    def post(self, request):
+        try:
+            data           = json.loads(request.body)
+            name           = data['name']
+            email          = data['email']
+            password       = data['password']
+            mobile_number  = data['mobile_number']
+            birthday       = data.get('birthday')
+
+            vaildNameRegex(name)
+            validEmailRegex(email)
+            validPasswordRegex(password)
+            validMobileRegex(mobile_number)
+            checkEmailExist(email)
+            checkMobileExist(mobile_number)
+
+            if birthday: 
+                validBirthdayRegex(birthday)
+
+            hashed_password = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt()).decode("utf-8")
+
+            User.objects.create(
+                name          = name,
+                email         = email,
+                password      = hashed_password,
+                mobile_number = mobile_number,
+                birthday      = birthday
+            )
+            return JsonResponse({'message': 'SUCCESS'}, status=201)
+
+        except KeyError as error:
+            return JsonResponse({'message': f'{error}'.strip("'")}, status=400)
+
+        except ValueError as error:
+            return JsonResponse({"message": f'{error}'.strip("'")}, status=400)
+        
+        except ObjectDoesNotExist as error:
+            return JsonResponse({"message": f'{error}'.strip("'")}, status=400)
+
+
+class LoginView(View):
+    def post(self, request):
+        try:
+            data             = json.loads(request.body)
+            email            = data['email']
+            password         = data['password']
+            user             = User.objects.get(email=email)
+
+            checkPassword(password, user.password)
+
+            token = jwt.encode({'id': user.id}, settings.SECRET_KEY, settings.ALGORITHM)
+
+            return JsonResponse({'message': 'SUCCESS', 'access_token': token})
+
+        except KeyError as error:
+            return JsonResponse({'message': f'{error}'.strip("'")}, status=400)
+        
+        except ValueError:
+            return JsonResponse({'message': 'INVALID_USER'}, status=401)
+
+        except ObjectDoesNotExist as error:
+            return JsonResponse({"message": f'{error}'.strip("'")}, status=400)
