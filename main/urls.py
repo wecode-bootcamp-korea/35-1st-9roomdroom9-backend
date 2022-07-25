7848ad6101a53e553ac545110a87039116e50b1c
jeum <eumjungmin1@gmail.com>
2022-07-25 10:58:52 +0900

    Add: MainPageView

--- main/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-from main.views import MainPageView
-
-urlpatterns = {
-    path('', MainPageView.as_view()),
-}
--- products/views.py
+++ products/views.py
@@ -3,6 +3,31 @@ from django.views import View
 
 from .models import Product, Category
 
+class MainPageView(View):
+    def get(self, request):
+        def get_list(products):
+            product_list =  [{
+                'id'      : product.id,
+                'name'    : product.name,
+                'price'   : product.price,
+                'is_green': product.is_green,
+                'is_best' : product.is_best,
+                'images'  : [{
+                    'id' : image.id,
+                    'url': image.url
+                    } for image in product.productimage_set.all()]
+            } for product in products]
+            return product_list
+
+        new_products   = Product.objects.all().order_by('-created_at')[:8]
+        best_products  = Product.objects.filter(is_best = True).order_by('-created_at')[:8] 
+        green_products = Product.objects.filter(is_green = True).order_by('-created_at')[:8]
+
+        return JsonResponse({
+            'new_products'  : get_list(new_products),
+            'best_products' : get_list(best_products),
+            'green_products': get_list(green_products)}, status=200)
+
 class ProductListView(View):
     def get(self, request, category_id=1000):
         if not Category.objects.filter(id=category_id).exists(): 
