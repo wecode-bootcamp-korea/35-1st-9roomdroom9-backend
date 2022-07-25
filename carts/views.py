d52536bc2ba340475f5f0defc22be8bc850c224e
fkelfk <fkelfk09@gmail.com>
2022-07-25 17:58:30 +0900

    FEAT : CartApp CartPatch Method

--- carts/views.py
+++ carts/views.py
@@ -10,18 +10,17 @@ from products.models import *
 
 class CartView(View):
     @accessCkeck
-    def delete(self, request):
+    def patch(self, request):
         try:
+            data     = json.loads(request.body)
             user_id  = request.user.id
             carts_id = request.GET.getlist('cart_id', None)
+            print(carts_id)
+            quantity = data['quantity']
             for cart_id in carts_id:
                 carts = Cart.objects.filter(id = cart_id, user_id = user_id)
-                carts.delete()
-
+                carts.update(quantity = quantity)
             return JsonResponse({'result': 'SUCCESS'}, status=200)
-        
-        except ValueError:
-            return JsonResponse({'message': 'INVALID_USER'}, status=401)
 
         except KeyError:
             return JsonResponse({'message': "KEY_ERROR"}, status=400)
