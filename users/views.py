7e9fc3b386043a5360c1b1f6a9da6bb833690b73
fkelfk <fkelfk09@gmail.com>
2022-07-25 13:00:34 +0900

    FEAT : Users app login view

--- users/views.py
+++ users/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.http  import JsonResponse
+from django.views import View
+
+from .models    import User
+from core.utils import *
+
+class LoginView(View):
+    def post(self, request):
+        try:
+            data     = json.loads(request.body)
+            email    = data['email']
+            password = data['password']
+            user     = User.objects.get(email=email)
+            checkPassword(password, user.password)
+            return JsonResponse({'message': 'SUCCESS', 'access_token': createToken(user.id)}, status=201)
+        except KeyError:
+            return JsonResponse({'message': 'KEY_ERROR'}, status=400)
+        except ValueError:
+            return JsonResponse({'message': 'INVALID_USER'}, status=401)
+        except User.DoesNotExist as error:
+            return JsonResponse({'message': f'{error}'.strip("'")}, status=404)
