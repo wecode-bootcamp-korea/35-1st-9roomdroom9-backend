d9ac8c01b8dc285f1d170759deb70082f8707813
fkelfk <fkelfk09@gmail.com>
2022-07-25 17:56:10 +0900

    FEAT : CartApp CartDelete Method

--- carts/views.py
+++ carts/views.py
@@ -10,18 +10,18 @@ from products.models import *
 
 class CartView(View):
     @accessCkeck
-    def get(self, request):
+    def delete(self, request):
         try:
-            user_id = request.user.id
-            carts   = Cart.objects.filter(user_id=user_id)
-            result  = [{
-                "cart_id"       : cart.id,
-                "quantity"      : cart.quantity,
-                "product_name"  : cart.product_option.product.name,
-                "product_image" : [image.url for image in cart.product_option.product.productimage_set.all()],
-                "product_price" : cart.product_option.product.price,
-                } for cart in carts]
-            return JsonResponse({'result': result}, status=200)
+            user_id  = request.user.id
+            carts_id = request.GET.getlist('cart_id', None)
+            for cart_id in carts_id:
+                carts = Cart.objects.filter(id = cart_id, user_id = user_id)
+                carts.delete()
+
+            return JsonResponse({'result': 'SUCCESS'}, status=200)
+        
+        except ValueError:
+            return JsonResponse({'message': 'INVALID_USER'}, status=401)
 
         except KeyError:
-            return JsonResponse({'message': 'Key_Error'}, status=400)
+            return JsonResponse({'message': "KEY_ERROR"}, status=400)
